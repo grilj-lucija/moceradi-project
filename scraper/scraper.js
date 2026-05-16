23ff870be9ca2d3a8ca7e6e5743122ae5616fcca
Lucija Grilj <lucija.grilj@student.um.si>
2026-05-16 14:54:35 +0200

    Update scraper.js - add sleep, error handling and Slovenian products filter

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -4,8 +4,8 @@ const axios = require('axios');
 const SUPABASE_URL = process.env.SUPABASE_URL;
 const SUPABASE_KEY = process.env.SUPABASE_SERVICE_KEY;
 
-async function fetchFoods() {
-    const url = 'https://world.openfoodfacts.org/api/v2/search?categories_tags=fruits&page_size=20&fields=product_name,nutriments'
+async function fetchFoods(page) {
+    const url = `https://world.openfoodfacts.org/api/v2/search?countries_tags=slovenia&page_size=50&page=${page}&fields=product_name,nutriments`
     const response = await axios.get(url, {
         headers:{
             'User-Agent': 'moceradi-project - Student project - lucija@example.com'
@@ -28,16 +28,34 @@ async function saveIngredients(name, calories) {
     );
 }
 
-async function main() {
-    const products = await fetchFoods();
-    for(const product of products){
-        const name = product.product_name;
-        const calories = product.nutriments['energy-kcal_100g'];
-    
-    if(!name || !calories) continue;
+function sleep(ms){
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-    await saveIngredients(name, calories);
-    console.log('Shranjeno:', name);
+async function main() {
+    for(let page = 1; page <= 80; page++){
+        console.log('Zajemam stran:', page);
+
+        try{
+        const products = await fetchFoods(page);
+
+        if(!products || products.length === 0) break;
+
+        for(const product of products){
+            if(!product.nutriments) continue;
+            const name = product.product_name;
+            const calories = product.nutriments['energy-kcal_100g'];
+    
+            if(!name || !calories) continue;
+
+            await saveIngredients(name, calories);
+            console.log('Shranjeno:', name);
+        }
+    } catch (err){
+        console.log('Napaka na strani', page, '-preskakujem');
+    }
+
+        await sleep(2000);
     }
 }
 
